0e77e945e1fd837e8da7f214bb9311f5566d1419
maria <mariakarla1996@gmail.com>
2022-06-15 23:50:51 -0400

    ok

--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import {Modal, show, Button} from 'react-bootstrap';
+import React, {useState} from 'react';
 const API_IMG="https://image.tmdb.org/t/p/w500/"
-const MovieBox = ({titel, poster_path, vote_average, release_date, overview})=>{
+
+
+const MovieBox = ({title, poster_path, vote_average, release_date, overview})=>{
+    
+    const [show, setShow]=useState(false);
+    const handleShow=()=>setShow(true);
+    const handleClose=()=>setShow(false);
+   
     return (
-      <div>
-        <h1>{titel}</h1>
-        <img src={API_IMG + poster_path}></img>
-        <p>{overview}</p>
-      </div>
+      <div className="card text-center border-success bg-white mb-3">
+          
+          <div className="card-body">
+          <img className="card-img-top" src={API_IMG + poster_path}/> 
+          <div className="card-body">
+                  <button type="button" className="btn btn-success"  onClick={handleShow} >Details</button>
+                  <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                       
+                      </Modal.Header>
+                      <Modal.Body>
+                      
+                      <div class="row">
+                        <div class="col-sm-6"><img className="card-img-top" style={{width:'15rem'}}src={API_IMG+poster_path}/>
+                        </div>
+                        <div class="col-sm-6"> 
+                        <h5> Title: {title}</h5>
+                        <h5>Release date: {release_date}</h5>
+                        <h5>Vote average: {vote_average}</h5>
+                        </div>                     
+                    </div>
+                    <br></br>
+                        <p>{overview}</p>
+                        
+                      
+                      
+                    
+                      
+                      </Modal.Body>
+                     
+                  </Modal>
+              </div>
+            </div>
+        </div>
     )
   }
   
